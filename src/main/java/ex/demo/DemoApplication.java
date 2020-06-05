ba172ecc5a0a46062cd3fa84c3eb346545ed58ba
99sujeong <ansdbs146@gmail.com>
2020-06-05 23:03:21 +0900

    Initial commit

---
+++ src/main/java/ex/demo/DemoApplication.java
@@ -0,0 +1,24 @@
+package ex.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+/*
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+ */
+}
